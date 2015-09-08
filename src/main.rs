fe99b9c776e835ff08944a942d09d5da7a89ab9c
Kevin Yap <me@kevinyap.ca>
2015-09-08 14:29:01 -0700

    Ignore program name given by env::args()
    Use static program name instead of determining it programmatically.

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,28 @@ use std::fs::File;
 use std::path::Path;
 
 
-fn print_usage(program: &str, opts: Options) {
-    let usage = format!("Usage: {} [options] <filename>", program);
-    print!("{}", opts.usage(&usage));
+
+fn print_usage(opts: Options) {
+    let usage = "Usage: ghoti [options] <filename>";
+    print!("{}", opts.usage(usage));
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
-
+    // Set up CLI options
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this help menu");
     opts.optflag("v", "version", "print version info");
 
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(f) => panic!(f.to_string()),
-    };
+    // Ignore the first element of env::args() (program name)
+    let args: Vec<String> = env::args().skip(1).collect();
+    let matches = opts.parse(&args).unwrap();
 
+    // Iterate through CLI flags
     if matches.opt_present("version") {
         println!("ghoti {}", env!("CARGO_PKG_VERSION"));
         return;
-    }
-
-    if matches.opt_present("help") || matches.free.is_empty() {
-        print_usage(&program, opts);
+    } else if matches.opt_present("help") || matches.free.is_empty() {
+        print_usage(opts);
         return;
     }
 
